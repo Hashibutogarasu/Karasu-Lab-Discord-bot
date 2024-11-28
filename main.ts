9f78b8c203f4ec0916b39c89955c945ccb4bb400
Hashibutogarasu <corvusmacrorhynchosjaponensis@gmail.com>
2024-11-28 10:53:06 +0900

    初期設定ファイルとDiscordenoボットのテンプレートを追加

---
+++ main.ts
@@ -0,0 +1,60 @@
+import { createBot, getBotIdFromToken, startBot, Intents, CreateSlashApplicationCommand, Bot, Interaction, InteractionResponseTypes } from "@discordeno/mod.ts";
+
+import "$std/dotenv/load.ts"
+
+interface SlashCommand {
+    info: CreateSlashApplicationCommand;
+    response(bot: Bot, interaction: Interaction): Promise<void>;
+};
+
+const BotToken: string = Deno.env.get("BOT_TOKEN")!;
+const HelloCommand: SlashCommand = {
+
+    info: {
+        name: "hello_world",
+        description: "こんにちはと返します。"
+    },
+
+    response: async (bot, interaction) => {
+        return await bot.helpers.sendInteractionResponse(interaction.id, interaction.token, {
+            type: InteractionResponseTypes.ChannelMessageWithSource,
+            data: {
+                content: "こんにちは",
+
+                flags: 1 << 6
+            }
+        });
+    }
+}
+
+
+const bot = createBot({
+    token: BotToken,
+    botId: getBotIdFromToken(BotToken) as bigint,
+
+    intents: Intents.Guilds | Intents.GuildMessages,
+
+
+    events: {
+
+        ready: (_bot, payload) => {
+            console.log(`${payload.user.username} is ready!`);
+        },
+        interactionCreate: async (_bot, interaction) => {
+            await HelloCommand.response(bot, interaction);
+        }
+    }
+});
+
+
+bot.helpers.createGlobalApplicationCommand(HelloCommand.info);
+
+
+bot.helpers.upsertGlobalApplicationCommands([HelloCommand.info]);
+
+
+await startBot(bot);
+
+Deno.cron("Continuous Request", "*/2 * * * *", () => {
+    console.log("running...");
+});
